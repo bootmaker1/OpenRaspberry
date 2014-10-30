db412e1e901b0fe9e408659c82a25bd0a0cc889c
Patrik <cizmarp1@gmail.com>
2014-10-30 15:01:26 +0100

    Updating my IP addr.

--- update_ip.sh
+++ update_ip.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo Updating my IP addr!
+echo "Checking my IP addr!"
 curl http://bot.whatismyipaddress.com/ > my_ip_addr.txt 
+cat my_ip_addr.txt
 git add -A
 git commit -a -m "Updating my IP addr."
 git push
