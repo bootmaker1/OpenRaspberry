9593e07619984dce2fd15ccc75d7d19efbb9d71e
Patrik <cizmarp1@gmail.com>
2014-10-31 08:39:43 +0100

    Updating my IP addr to 82.142.115.209 at Fri Oct 31 08:39:43 CET 2014

--- update_ip.sh
+++ update_ip.sh
@@ -4,6 +4,9 @@
 newip=$(curl -s http://bot.whatismyipaddress.com/)
 oldip=$(cat my_ip_addr.txt)
 
+#detect all new or deleted files
+git add -A
+
 change=$(git status -s)
 
 if [ "$newip" != "$oldip" ] || [ "$change" != "" ]; then
@@ -13,14 +16,12 @@ echo -e "$(date): Checking my public IP addr now!\n"
 
 #print my new IP addr
 cat my_ip_addr.txt
+
 #print one free line after the text
 echo -e '\n'
 
-#detect all new or deleted files
-git add -A
-
 #-q means quiet mode - no verbose
-git commit -q -a -m "Updating my IP addr."
+git commit -q -a -m "Updating my IP addr to $newip at $(date)"
 git push -q
 
 echo -e "\nDone!"
