6309d814e6c98de58639950b4c918bf08e591fe4
Patrik <cizmarp1@gmail.com>
2014-10-31 08:42:16 +0100

    Updating my IP addr to 82.142.115.209 at Fri Oct 31 08:42:16 CET 2014

--- update_ip.sh
+++ update_ip.sh
@@ -7,8 +7,10 @@ oldip=$(cat my_ip_addr.txt)
 #detect all new or deleted files
 git add -A
 
+#detect if something has changed in this git folder
 change=$(git status -s)
 
+#if ip addr differs or if there is some change in folder
 if [ "$newip" != "$oldip" ] || [ "$change" != "" ]; then
 
 #-e means that i detects \n
